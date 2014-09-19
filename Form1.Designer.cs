566fca95f95abf3d87ddb7d4d8a47bbedfec50d0
Norman <el.norman01@gmail.com>
2014-09-19 11:28:45 -0400

    First Commit -Adding the project

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.btDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.DownloadBar = new System.Windows.Forms.ProgressBar();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(12, 92);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(74, 32);
+            this.btBrowse.TabIndex = 0;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // btDownload
+            // 
+            this.btDownload.Location = new System.Drawing.Point(186, 92);
+            this.btDownload.Name = "btDownload";
+            this.btDownload.Size = new System.Drawing.Size(84, 32);
+            this.btDownload.TabIndex = 1;
+            this.btDownload.Text = "Download";
+            this.btDownload.UseVisualStyleBackColor = true;
+            this.btDownload.Click += new System.EventHandler(this.btDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Filepath:";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(81, 36);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(189, 22);
+            this.tbUrl.TabIndex = 4;
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(81, 64);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(189, 22);
+            this.tbFile.TabIndex = 5;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // DownloadBar
+            // 
+            this.DownloadBar.Location = new System.Drawing.Point(13, 131);
+            this.DownloadBar.Name = "DownloadBar";
+            this.DownloadBar.Size = new System.Drawing.Size(257, 23);
+            this.DownloadBar.TabIndex = 6;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Enabled = false;
+            this.btCancel.Location = new System.Drawing.Point(98, 92);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 32);
+            this.btCancel.TabIndex = 7;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 156);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.DownloadBar);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btDownload);
+            this.Controls.Add(this.btBrowse);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.Button btDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ProgressBar DownloadBar;
+        private System.Windows.Forms.Button btCancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Downloader
+{
+    public partial class Form1 : Form
+    {
+        static private WebClient client;
+        static private Uri url;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new WebClient();
+
+            client.Disposed +=
+            (s, e) =>
+            {
+                DownloadBar.Value = 0;
+                client.CancelAsync();
+                while (client.IsBusy) ;
+                try
+                {
+                    if (File.Exists(this.tbFile.Text))
+                        File.Delete(this.tbFile.Text);
+                }
+                catch(Exception ex)
+                {
+                    /*FAIL!*/
+                }
+                this.btBrowse.Enabled = true;
+                this.btDownload.Enabled = true;
+                this.btCancel.Enabled = false;
+            };
+
+            client.DownloadDataCompleted += 
+            (s, e) =>
+            {
+                DownloadBar.Value = 0;
+                this.btBrowse.Enabled = true;
+                this.btDownload.Enabled = true;
+                this.btCancel.Enabled = false;
+                this.Refresh();
+            };
+
+            client.DownloadProgressChanged +=
+            (s, e) =>
+            {
+                DownloadBar.Value = e.ProgressPercentage;
+                this.Refresh();
+            };
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            if(!tbUrl.Text.Equals(""))
+            {
+                url = new Uri(tbUrl.Text);
+                saveFileDialog.FileName = Path.
+                    GetFileName(url.AbsolutePath);
+                saveFileDialog.AddExtension = true;
+            }
+            saveFileDialog.ShowDialog();
+        }
+
+        private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            this.tbFile.Text = saveFileDialog.FileName;
+        }
+
+        private void btDownload_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                this.btBrowse.Enabled = false;
+                this.btDownload.Enabled = false;
+                this.btCancel.Enabled = true;
+                client.DownloadFileAsync(url, this.tbFile.Text);
+            }
+            catch(Exception ex)
+            {
+                client.Dispose();
+                MessageBox.Show(this, ex.ToString());
+            }
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            client.Dispose();
+            this.btCancel.Enabled = false;
+        }
+    }
+}
